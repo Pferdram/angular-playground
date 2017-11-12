b40af46407092f72b796ebdf2b54031d545540d4
Bertram Holthaus <bertram.holthaus@ingentis.com>
2017-11-12 18:53:23 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
+
 
 import { AppComponent } from './app.component';
+import { MaterialWrapperModule } from './material-wrapper/material-wrapper.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialWrapperModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material-wrapper/material-wrapper.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import {
+  MatSelectModule,
+  MatAutocompleteModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatGridListModule,
+  MatIconModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatRadioModule,
+  MatInputModule
+} from '@angular/material';
+
+@NgModule({
+  exports: [
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatRadioModule,
+    MatInputModule
+  ]
+})
+export class MaterialWrapperModule { }
